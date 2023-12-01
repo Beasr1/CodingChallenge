a1157bc8f2f3f81e7b4825ae1c83e398834ddd08
Beasr1 <d.ishaan001@gmail.com>
2023-12-02 00:35:17 +0530

    imporved priority queue to accept custom comparator

--- 3_compressionTool/cclibrary/priority_queue_module.py
+++ 3_compressionTool/cclibrary/priority_queue_module.py
@@ -2,12 +2,26 @@
 # using Queue.
 # Class inherits from Object
 # whereas now python it get inherited implictly
+
+'''
+my custom comparator
+(top,newElement)
+if true then swap
+else do not
+
+so
+lambda x,y : x<y #default x<y
+top<ele than swap means max heap
+'''
 class PriorityQueue(object):
-    def __init__(self,arr=[]):
+    def __init__(self,arr=None,comparator=None):
         #default arr is empty if not provided
         self.queue = []
-        for i in range(len(arr)):
-            self.insert(arr[i])
+        self.compare=comparator
+        if not(comparator): self.compare=lambda top,ele : top<ele #default x<y
+        if arr:
+            for i in range(len(arr)):
+                self.insert(arr[i])
  
     def __str__(self):
         return ' '.join([str(i) for i in self.queue])
@@ -15,23 +29,27 @@ class PriorityQueue(object):
     # for checking if the queue is empty
     def isEmpty(self):
         return len(self.queue) == 0
+    
+    def __swap(self,i,j):
+        self.queue[i],self.queue[j]=self.queue[j],self.queue[i]
+
  
     # for inserting an element in the queue
     def insert(self, data):
         self.queue.append(data)
-
-        #now reorder up
-        # 0 : 1,2  1 : 3,4
+        self.__heapify_up() #now reorder up
+        
+    def __heapify_up(self):
         curr=len(self.queue)-1
         parent=(int)((curr-1)/2)
 
         #making max heap so child is more than parent
-        while (curr>0 and self.queue[parent]<self.queue[curr]):
-            self.queue[parent],self.queue[curr]=self.queue[curr],self.queue[parent]
+        #self.queue[parent]<self.queue[curr]
+        while (curr>0 and self.compare(self.queue[parent],self.queue[curr])):
+            self.__swap(parent,curr)
             curr=parent
             parent=(int)((curr-1)/2)
 
-        print(self)
  
     # for popping an element based on Priority
     #swap front with back
@@ -39,15 +57,16 @@ class PriorityQueue(object):
     # heapify it again
 
     def delete(self):
+        if(self.isEmpty()): return None
+
         item =self.queue[0]
-
-        n=len(self.queue)
-        self.queue[0],self.queue[n-1]=self.queue[n-1],self.queue[0]
+        self.__swap(0,len(self.queue)-1)
         self.queue.pop()
+        self.__heapify_down() #now stabalize the top : for max heap
+        return item
+    
+    def __heapify_down(self):
         n=len(self.queue) #new size 
-
-        #now stabalize the top
-        #for max heap
         curr=0
         
         while( curr<n):
@@ -55,35 +74,22 @@ class PriorityQueue(object):
             right=2*curr+2
             if(left>=n): break
             if(right>=n):
-                if(self.queue[left]>self.queue[curr]):
-                    self.queue[curr],self.queue[left]=self.queue[left],self.queue[curr]
+                #self.queue[curr]<self.queue[left]
+                if(self.compare(self.queue[curr],self.queue[left])):
+                    self.__swap(left,curr)
                     curr=left
                     continue
             
             maxIndex=left
-            if(self.queue[left]<self.queue[right]): maxIndex=right
-            if(self.queue[maxIndex]>self.queue[curr]):
-                self.queue[curr],self.queue[maxIndex]=self.queue[maxIndex],self.queue[curr]
+            #self.queue[left]<self.queue[right] : default
+            if(self.compare(self.queue[left],self.queue[right])): maxIndex=right
+            #self.queue[curr]<self.queue[maxIndex] : default
+            if(self.compare(self.queue[curr],self.queue[maxIndex]) ):
+                self.__swap(maxIndex,curr)
                 curr=maxIndex
                 continue
-        return item
 
 
-
-        
-    # brute implmentation
-    def delete_brute(self):
-        try:
-            max_val = 0
-            for i in range(len(self.queue)):
-                if self.queue[i] > self.queue[max_val]:
-                    max_val = i
-            item = self.queue[max_val]
-            del self.queue[max_val]
-            return item
-        except IndexError:
-            print()
-            exit()
 
 
 if __name__ == '__main__':
